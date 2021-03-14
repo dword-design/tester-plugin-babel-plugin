20deff56bcaa25f3291cbb274875d2ce7c9999ec
Sebastian Landwehr <info@dword-design.de>
2021-03-14 15:20:20 +0100

    feat: init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { transform } from '@babel/core'
+import { property } from '@dword-design/functions'
+import expect from 'expect'
+import P from 'path'
+
+export default options => ({
+  transform: test => async () =>
+    expect(
+      transform(test.from, {
+        babelrc: false,
+        filename: P.join(process.cwd(), 'index.js'),
+        ...options,
+      })
+        |> await
+        |> property('code')
+    ).toEqual(test.to),
+})
---
+++ src/index.spec.js
@@ -0,0 +1,24 @@
+import { endent } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+
+import self from '.'
+
+export default tester(
+  {
+    valid: {
+      from: '1 |> x => x * 2',
+      to: endent`
+        var _;
+        
+        _ = 1, _ * 2;
+      `,
+    },
+  },
+  [
+    self({
+      plugins: [
+        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'fsharp' }],
+      ],
+    }),
+  ]
+)
